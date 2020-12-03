de67ba3f03ae8cb6fc8f139e319ea97e01d40152
Ding <mlin702651@gmail.com>
2020-12-03 23:16:24 +0800

    1203

---
+++ Assets/Script/CameraChange.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace Cinemachine.Examples
+{
+    [AddComponentMenu("")] // Don't display in add component menu
+    public class CameraChange : MonoBehaviour
+    {
+        
+        public CinemachineVirtualCameraBase switchCam; //Camera
+        
+        void Start()
+        {
+           
+        }
+
+        void OnTriggerEnter(Collider col)
+        {
+            if ( switchCam )
+            {
+                if (Camera.main.GetComponent<CinemachineBrain>().ActiveVirtualCamera.Name != switchCam.Name)
+                {     
+                    switchCam.VirtualCameraGameObject.SetActive(false); //先禁用再启用，得到切换效果
+                    switchCam.VirtualCameraGameObject.SetActive(true);
+                }
+            }
+        }
+
+       
+
+    }
+}
---
+++ Assets/Script/SetGuide.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetGuide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        gameObject.SetActive(false);
+        
+    }
+}
